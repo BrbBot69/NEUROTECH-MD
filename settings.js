542e44d81358de56d3ce5bbef4dd52fb2fb73a49
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-12 19:38:18 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "BALLAS𝗑ᴾᴿᴼ",
   botOwner: '♤𝗑ᴾᴿᴼ♧', // Your name
-  ownerNumber: '27767494368', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '27818232269', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
